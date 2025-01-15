4c4c6b91fa19ee91cd1c80d7b3e5ac593e1b76ca
boriskalika <boris.kalikaa@gmail.com>
2025-01-15 01:29:48 +0100

    implementation 'till 1.11 [misunderstood that you can still individually push the exercises just make sure to only SUBMIT when everything is pushed...-sorry

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,7 +1,182 @@
 import { useState } from 'react'
 
 const App = () => {
+  const [value, setValue] = useState(10)
+  
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+  
+  return (
+    <div>
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
+    </div>
+  )
+}
 
+export default App
+
+/*
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)    
+    setTotal(left + updatedRight  )
+  }
+
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <p>total {total} </p>
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+
+export default App
+*/
+/*d2)
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)    
+    setTotal(left + updatedRight  )
+  }
+
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>total {total} </p>
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+
+export default App
+*/
+
+/*
+d)
+const App = () => {
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftclick = () => {
+    const newClicks = {
+      ...clicks,
+      left: clicks.left +1
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      ...clicks,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
+
+  return (
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftclick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+    </div>
+  )
+}
+
+export default App
+
+
+
+1.c
+const App = () => {
+ 
   const Display = ({ counter }) => <div>{counter}</div>
 
   const Button = ({onHulkSmash, name}) => <button onClick={onHulkSmash}> {name} </button>
@@ -35,8 +210,8 @@ const App = () => {
       <Button onHulkSmash={decreaseByOne} name='minus' />
       <Button onHulkSmash={setToZero} name='zero' />
       <Button onHulkSmash={randomNum} name='random' />
+      
     </div>
   )
 }
-
-export default App
+*/
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,120 @@
+import { useState } from 'react'
+
+const Statistics = ({good, neutral, bad, total, average, pospercentage}) => { 
+  return(
+    <div>
+    <StatisticLine name="good" value={good} />
+    <StatisticLine name="neutral" value={neutral} />
+    <StatisticLine name="bad" value={bad} />
+    <StatisticLine name="all" value={total} />
+    <StatisticLine name="average" value={average} />
+    <StatisticLine name="positive" value={pospercentage} />
+    </div>
+  )
+}
+
+const StatisticLine = ({name, value}) => {
+  return (
+    <table style={{ width: "300px", tableLayout:'fixed'}}>
+        <tbody>
+            <tr>
+              <td>{name}</td>
+              <td>{name === "positive" ? `${value} %` : value}</td>
+            </tr>
+        </tbody>
+    </table>
+  )
+}
+
+const Button =({onHulkSmash, name}) =>{ 
+return(
+  <button onClick={onHulkSmash}> {name}</button>
+)
+}
+const App = () => {
+  // save clicks of each button to its own state
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const countGood = () =>{
+    setGood(good + 1)
+    console.log('good counter increased! Value before:', good)
+  }
+
+  const countNeutral = () => {
+    setNeutral(neutral + 1)
+    console.log('neutral counter increased! Value before:', neutral)
+  }
+
+  const countBad = () =>{
+    setBad(bad + 1)
+    console.log('bad counter increased! Value before:', bad)
+  }
+  
+  const total = good + neutral + bad
+  const pospercentage = total === 0 ? 0 : ((good/total) * 100).toFixed(10)
+  const score = good - bad
+  const average = total === 0 ? 0 : (score/total).toFixed(10)
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onHulkSmash={countGood} name='good' />
+      <Button onHulkSmash={countNeutral} name='neutral' />
+      <Button onHulkSmash={countBad} name='bad' />
+      <h1>statistics</h1>
+      {total === 0 ? (
+        <p>No feedback given</p>
+        ) : (
+        <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} pospercentage={pospercentage}/>
+        )}
+    </div>
+  )
+}
+
+export default App
+
+
+/*
+const App = () => {
+ 
+  const Display = ({ counter }) => <div>{counter}</div>
+
+  const Button = ({onHulkSmash, name}) => <button onClick={onHulkSmash}> {name} </button>
+
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () =>{ 
+    setCounter(counter + 1)
+    console.log('increased! Value before:', counter)
+  }
+
+  const decreaseByOne = () => {
+    setCounter(counter - 1)
+    console.log('decreased! Value before:', counter)
+  }
+
+  const setToZero = () => {
+    setCounter(0)
+    console.log('resetted! Value before:', counter)
+  }
+
+  const randomNum = () => {
+    setCounter(Math.random())
+    console.log('RNG HUZAHAHHH! Value before:', counter)
+  }
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onHulkSmash={increaseByOne} name='plus' />
+      <Button onHulkSmash={decreaseByOne} name='minus' />
+      <Button onHulkSmash={setToZero} name='zero' />
+      <Button onHulkSmash={randomNum} name='random' />
+      
+    </div>
+  )
+} */
